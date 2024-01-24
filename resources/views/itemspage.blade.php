402dbb79ca9b9b67cca9188638db5a17d924e006
Mohammed-Alghumayti <m7md3id.10@gmail.com>
2024-01-24 17:00:14 +0300

    add admin show items

--- resources/views/itemspage.blade.php
+++ resources/views/itemspage.blade.php
@@ -1,6 +1,46 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="main-banner">
+    <div class="container">
+        <div class="row">
+            
+        <div class="section most-played">
+    <div class="container">
+      <div class="row">
 
+        <div class="col-lg-6">
+          <div class="section-heading">
+            <h6>TOP GAMES</h6>
+            <h2>Most Played</h2>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="main-button">
+            <a href="shop.html">View All</a>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-6 col-sm-6">
+          <div class="item">
+            <div class="thumb">
+              <a href="product-details.html"><img src="assets/images/welcomePage/banner-bg1.png" alt=""></a>
+            </div>
+            <div class="down-content">
+              <span class="category">Adventure</span>
+              <h4>Assasin Creed</h4>
+              <a href="product-details.html">Explore</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+        </div>
+    </div>
+</div>
 
 @endsection
